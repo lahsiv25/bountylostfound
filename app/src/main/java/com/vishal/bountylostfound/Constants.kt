91d1ec89b6ae27a8b8927cc3bb003c67d962df4e
vishal <lahsiv250601@gmail.com>
2022-08-11 20:27:30 +0530

    sixth commit

--- app/src/main/java/com/vishal/bountylostfound/Constants.kt
+++ app/src/main/java/com/vishal/bountylostfound/Constants.kt
@@ -2,6 +2,8 @@ package com.vishal.bountylostfound
 
 class Constants {
     companion object{
+        const val l = "jjjjjjjjjj"
+        var b = 44
         const val REFERENCE_URL = "https://bountylostfound-default-rtdb.asia-southeast1.firebasedatabase.app/git "
     }
 }
